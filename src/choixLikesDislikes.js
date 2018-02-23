f4fbaab5e381179cd7edd24c54041bbb540a0597
Maxime <contact@maximepawlak.fr>
2018-02-23 11:30:30 +0100

    Fix if localstorage is empty.

--- src/choixLikesDislikes.js
+++ src/choixLikesDislikes.js
@@ -26,7 +26,7 @@ class ChoixLikesDislikes extends Component {
 
     //s'il y a déjà des éléments dans le localStorage correspondant à des médias likés, on les récupère dans une liste
     if (localStorage.getItem("likes")) {
-      let likes1 = JSON.parse(localStorage.getItem("likes"))||[];
+      let likes1 = JSON.parse(localStorage.getItem("likes")) || [];
       likes1.forEach(function(element) {
 
         likes.push(element);
@@ -55,36 +55,40 @@ class ChoixLikesDislikes extends Component {
   }
 
   render() {
+    const firstMedia = this.props.listedemedias[0];
 
     return (
       <Container>
 			<div className="container">
-				<div class="card mb-4 box-shadow">
-	          <div class="card-header">
-	            <h4 class="my-0 font-weight-normal">{this.props.listedemedias[0].original_title}</h4>
-	          </div>
-	          <div class="card-body">
-	            <h1 class="card-title pricing-card-title">{this.props.listedemedias[0].original_title}</h1>
-	            <ul class="list-unstyled mt-3 mb-4">
-	              
-	              <li><h3>Tagline:</h3>{this.props.listedemedias[0].tagline}</li>
-	              <li><h3>Synopsis:</h3>{this.props.listedemedias[0].overview}</li>
-	              <li><h3>Genre:</h3> {this.props.listedemedias[0].genre[0].name}</li>
-	              <li><h3>Release:</h3>{this.props.listedemedias[0].genre[0].release}</li>
-	              
-	              
+        { firstMedia ? (
+            <div class="card mb-4 box-shadow">
+                <div class="card-header">
+                  <h4 class="my-0 font-weight-normal">{firstMedia.original_title}</h4>
+                </div>
+                <div class="card-body">
+                  <h1 class="card-title pricing-card-title">{firstMedia.original_title}</h1>
+                  <ul class="list-unstyled mt-3 mb-4">
+                    
+                    <li><h3>Tagline:</h3>{firstMedia.tagline}</li>
+                    <li><h3>Synopsis:</h3>{firstMedia.overview}</li>
+                    <li><h3>Genre:</h3> {firstMedia.genre[0].name}</li>
+                    <li><h3>Release:</h3>{firstMedia.genre[0].release}</li>
+                    
+                    
 
-	              
-	              
-	            </ul>
-	            
-					    	
-					    	 
-					<button type="button" class="btn btn-success btn-lg btn-block" onClick={this.ajoutListeLikes.bind(this)}>Like</button>
-					<button type="button" class="btn btn-danger btn-lg btn-block" onClick={this.dislikeMedia.bind(this)}>Dislike</button>
-					
-	        </div>
-	        </div>
+                    
+                    
+                  </ul>
+                  
+                    
+                    
+              <button type="button" class="btn btn-success btn-lg btn-block" onClick={this.ajoutListeLikes.bind(this)}>Like</button>
+              <button type="button" class="btn btn-danger btn-lg btn-block" onClick={this.dislikeMedia.bind(this)}>Dislike</button>
+              
+              </div>
+              </div>
+          ): "" 
+        }
 		</div>
 		</Container>
       );
